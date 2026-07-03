d62135895bda320a4c05e018ff2d409bbd3889f6
Ankur Rawat <ankurrawat2005@gmail.com>
2026-07-03 12:43:40 +0530

    lent

--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
+let age = "fher" ;
+console.log(typeof age)
